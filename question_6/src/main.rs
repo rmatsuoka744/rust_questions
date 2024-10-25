8f1bb70c191626ade828cda48eea100133d55e35
rmatsuoka744 <r.matsuoka@freebit.net>
2024-10-25 18:22:04 +0900

    question_6

--- question_6/src/main.rs
+++ question_6/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::task;
+use tokio::time::{sleep, Duration};
+
+#[tokio::main]
+async fn main() {
+    let durations: Vec<Duration> = vec![
+        Duration::from_secs(1),
+        Duration::from_secs(2),
+        Duration::from_secs(3),
+    ];
+
+    let tasks: Vec<_> = durations
+        .into_iter()
+        .enumerate()
+        .map(|(i, duration)| {
+            task::spawn(async move {
+                sleep(duration).await;
+                println!("This is Task {}", i + 1);
+            })
+        })
+        .collect();
+
+    for task in tasks {
+        task.await.unwrap();
+    }
 }
